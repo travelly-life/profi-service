e92e901b45510026813674feb4e3d5015bed0ae4
blackMaster007 <vturchkov@yandex.ru>
2023-07-11 21:04:30 +0300

    Добавил таблицу ps_services

---
+++ src/main/java/com/travelly/profiservice/entity/Services.java
@@ -0,0 +1,55 @@
+package com.travelly.profiservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "ps_services")
+
+public class Services {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+
+    @Column(name = "user_id")
+    @GeneratedValue(generator = "UUID")
+    private UUID userId;
+
+    @Column(name = "category_id")
+    @GeneratedValue(generator = "UUID")
+    private UUID categoryId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "contact")
+    private String contact;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+}
