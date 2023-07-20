84f3c9864ba8f2e9db8664f2593c46a23eb41c8b
vladimir burtsev <amakusl2@gmail.com>
2023-07-20 15:15:16 +0300

    add Category entity, changeset to create category table & add liquibase dependensies on gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.1.1"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("org.liquibase.gradle") version "2.0.4"
 }
 
 group = "com.travelly"
@@ -24,10 +25,14 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.liquibase:liquibase-core")
+    implementation("in.yajnesh.util:java-util:1.0.0")
+	implementation("util:util:0.1.0-SNAPSHOT")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("org.postgresql:postgresql")
+	liquibaseRuntime("org.liquibase', name: 'liquibase-core', version: '4.23.0")
+	liquibaseRuntime("org.liquibase:liquibase-groovy-dsl:2.1.1")
+	liquibaseRuntime("org.postgresql', name: 'postgresql', version: '42.6.0")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
